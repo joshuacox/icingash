d91e7944441d73f2fadb95738f59510a6ac1b91e
Josh Cox <josh@webhosting.coop>
2015-04-05 18:08:23 -0500

    initial import

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp /etc/logstash/template.d/logstash.tpl /opt/logstash/conf.d/logstash.conf
+sed -i "s/REDIS_PORT_6379_TCP_ADDR/$REDIS_PORT_6379_TCP_ADDR/" /opt/logstash/conf.d/logstash.conf
+sed -i "s/REDIS_PORT_6379_TCP_PORT/$REDIS_PORT_6379_TCP_PORT/" /opt/logstash/conf.d/logstash.conf
+
+/bin/bash /app/bin/boot
